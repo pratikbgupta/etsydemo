565796c001d2269bd82d632b97ef5c3dc60d9803
Pratik Gupta <pratikbgupta@gmail.com>
2015-01-05 20:12:10 -0600

    default image fix

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,8 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
-	else	
-		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
+	else
+		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "https://www.dropbox.com/s/zesciod6pigspzg/default.jpg?dl=0",
 					  :storage => :dropbox,
     				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     				  :path => ":style/:id_:filename"
