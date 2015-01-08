df951f6b2657255a61b6bb4c4b153b80448d4fa4
Pratik Gupta <pratikbgupta@gmail.com>
2015-01-07 18:51:51 -0600

    change link for banner

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
 	else
-		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "https://dl.dropboxusercontent.com/1/view/5r96wemv8us0i6d/Apps/EtsyDemo2015/default/default.jpg",
+		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "https://dl.dropboxusercontent.com/s/zesciod6pigspzg/default.jpg",
 					  :storage => :dropbox,
     				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     				  :path => ":style/:id_:filename"
