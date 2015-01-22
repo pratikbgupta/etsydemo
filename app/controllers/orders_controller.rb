18db02942a160b6a9a0c08addccd69467496ff81
Pratik Gupta <pratikbgupta@gmail.com>
2015-01-21 18:56:03 -0600

    charge credit cards

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,6 +27,20 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+    Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@listing.price * 100).floor,
+        :currency => "usd",
+        :card => token
+        )
+      flash[:notice] = "Thanks for ordering!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
     @order.save
     respond_with(@order, :location => root_url)
   end
