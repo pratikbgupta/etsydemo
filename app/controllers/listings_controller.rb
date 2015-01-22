f49314a78e46f9153b88bea1f5f25ca12ee93fa3
Pratik Gupta <pratikbgupta@gmail.com>
2015-01-22 13:54:27 -0600

    transfer funds to seller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -33,6 +33,20 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id    # devise provides current_user to know the current user information
 
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
+    end
+
+    current_user.recipient = recipient.id
+    current_user.save
+
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
